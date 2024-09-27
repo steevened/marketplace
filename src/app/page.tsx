cdabf09713a1012901b5e8ac7cc2b4c2d0bfc976
steevened <alvaradosteven315@gmail.com>
2024-09-26 22:47:00 -0500

    configure cloudflare pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -30,7 +32,7 @@ export default function Home() {
             <Link
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
-              className="fex gap-2"
+              className="fex gap-1.5"
               rel="noopener noreferrer"
             >
               <Image
@@ -43,7 +45,7 @@ export default function Home() {
               Deploy now
             </Link>
           </Button>
-          <Button size={"lg"} variant={"outline"} isLoading={true}>
+          <Button size={"lg"} variant={"outline"}>
             Read our docs
           </Button>
         </div>
