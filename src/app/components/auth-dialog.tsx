d8b9b426b2a6dce4c2ed5e939405a2031b5e732f
steevened <alvaradosteven315@gmail.com>
2024-10-11 23:54:50 -0500

    style login dialog

--- src/app/components/auth-dialog.tsx
+++ src/app/components/auth-dialog.tsx
@@ -7,6 +7,8 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import SignInForm from "./sign-in-form";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 export default function AuthDialog() {
   return (
@@ -18,7 +20,29 @@ export default function AuthDialog() {
         <DialogHeader>
           <DialogTitle>See more on Marketplace</DialogTitle>
         </DialogHeader>
-        <SignInForm />
+        <div className="grid gap-1.5">
+          <div className="grid gap-3">
+            <SignInForm />
+            <div className="text-center">
+              <Link href={"forgot-password"}>
+                <small className="">Forgot password?</small>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="w-full">
+              <Separator />
+            </div>
+            o
+            <div className="w-full">
+              <Separator />
+            </div>
+          </div>
+
+          <div className="text-center">
+            <Button size={"sm"}>Create new account</Button>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
